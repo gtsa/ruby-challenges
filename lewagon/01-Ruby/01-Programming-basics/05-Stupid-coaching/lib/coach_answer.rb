ed86591c52928f27d625d5289eebabfe6e4b04a3
geo tsa <gtsagiannis@gmail.com>
2024-11-01 16:54:50 +0000

    01-Programming Basics challenges solutions til 04

--- lewagon/01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ lewagon/01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,5 +1,10 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message == "I am going to work right now!"
+    exit
+  else
+    puts "Dont care"
+  end
 end
 
 def coach_answer_enhanced(your_message)
--- lewagon/01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ lewagon/01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,5 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+puts "What's up?"
+my_answer = gets.chomp
+coach_answer(my_answer)
