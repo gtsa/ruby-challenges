7fc71859fb431ff65e6e6afbd909bd60315381f7
geo tsa <gtsagiannis@gmail.com>
2024-11-03 19:10:04 +0000

    02-Flow Array challenges solutions

--- lewagon/01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ lewagon/01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -1,11 +1,24 @@
 require_relative "wagon_sort"
-
 # TODO: Ask the user about students to add to the Wagon.
 #       Remember, to read an input from the command line, use:
-#       - `gets`:  https://ruby-doc.org/core-3.1.2/Kernel.html#method-i-gets
-#       - `chomp`: https://ruby-doc.org/core-3.1.2/String.html#method-i-chomp
+#       - `gets`:  https://ruby-doc.org/core-2.7.5/Kernel.html#method-i-gets
+#       - `chomp`: https://ruby-doc.org/core-2.7.5/String.html#method-i-chomp
+wagon_list = []
+puts "Type a student name:"
+name = gets.chomp
+until name == ""
+  wagon_list.push(name)
+  puts "Type another student name or press enter to finish:"
+  name = gets.chomp
+end
+
+
 
 
 
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
+puts "Congratulations! Your Wagon has #{wagon_list.size} student#{wagon_list.size > 1 ? 's' : ''}:\n"
+result = wagon_sort(wagon_list)
+print result.size > 1 ? result[0..-2].join(', ') : result[0]
+puts result.size > 1 ? " and #{result[-1]}" : ""
